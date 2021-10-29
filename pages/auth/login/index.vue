46da034ef56a5ba76cae29836e179042f1818857
sallymkabole <sllmusanga7@gmail.com>
2021-10-29 12:08:22 +0300

    Add components to Login Page

--- pages/auth/login/index.vue
+++ pages/auth/login/index.vue
@@ -1,13 +1,229 @@
 <template>
-<v-card class="overflow-hidden">
-    <v-card-text>This is the Login page</v-card-text>
-  </v-card>
+  <v-layout column justify-center align-center class="main-layout" >
+      <v-layout class="right-nav">
+    <v-card class="login">
+        <v-layout  class="auth-card"  justify-center align-center >
+      <h3 class=" black--text font-weight-bold ">
+        Login to <a  href="/">Procquire</a>
+      </h3>
+      <v-form v-model="valid">
+       
+
+        <h3 class="text-left">Email</h3>
+        <v-text-field
+        dense
+          v-model="userInfo.email"
+          :rules="[emailrules.required, emailrules.email]"
+          class=""
+          color="#45a622"
+          outlined
+          placeholder="Email"
+        ></v-text-field>
+        <h3 class="text-left">Password</h3>
+        <v-text-field
+        dense
+          v-model="userInfo.password"
+          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+          :passrules="[rules.required, rules.min]"
+          :type="show1 ? 'text' : 'password'"
+          name="input-10-1"
+          hint="At least 8 characters"
+          counter
+          @click:append="show1 = !show1"
+          color="#45a622"
+          outlined
+          placeholder="Password"
+        ></v-text-field>
+       <NuxtLink class="mb-12 float-right" to="/auth/signup">
+      Forgot Password
+    </NuxtLink>
+    
+        <v-btn
+          class="btn1"
+          block
+          color="#45A622"
+          @click="registerUser"
+          :disabled="!valid"
+        >
+         LOG IN</v-btn
+        >
+        <h4 class="mt-2 float-right" to="/auth/signup">
+     Don’t have a Procquire account?<NuxtLink class="mb-2 float-right" to="/auth/signup">
+     Signup
+    </NuxtLink>
+    </h4>
+    
+      </v-form>
+        </v-layout>
+    </v-card>
+      </v-layout>
+  </v-layout>
 </template>
-
 <script>
+import axios from "axios";
 export default {
-  name:'login',
-  layout:'default',
-}
-</script>
+  name: "login",
+  layout: "default",
+  components: {
+  },
+  data() {
+    return {
+      radios: null,
+    
+     
+      errors: [],
+      valid: false,
+      userInfo: {
+        
+        email: "",
+      
+        password: "",
+      
+      },
+      country: null,
+      form: {
+        min: 18,
+        max: 100,
+      },
+      number: 0,
+      emailrules: {
+        required: (value) => !!value || "Required.",
+        counter: (value) => value.length <= 20 || "Max 20 characters",
+        email: (value) => {
+          const pattern =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Invalid e-mail.";
+        },
+      },
+      show1: false,
+      
+      passrules: {
+        required: (value) => !!value || "Required.",
+        min: (v) => v.length >= 8 || "Min 8 characters",
+      },
+      password: "Password",
+      rules: {
+        required: (value) => !!value || "Required.",
+        min: (v) => v.length >= 8 || "Min 8 characters",
+        emailMatch: () => `The email and password you entered don't match`,
+        min: (v) => v >= this.form.min || `The Min is ${this.form.min}`,
+        max: (v) => v <= this.form.max || `The Max is ${this.form.max}`,
+      },
+    };
+  },
+  mounted() {
+  
+  },
+  methods: {
+    
+    checkForm: function (e) {
+      if (this.fullname && this.email) return true;
+      this.errors = [];
+      if (!this.fullname) this.errors.push("Fullname required.");
+      if (!this.email) this.errors.push("Email required.");
+      e.preventDefault();
+    },
 
+   
+    registerUser() {
+      console.log(this.userInfo)
+      axios
+        .post("https://dmserver.herokuapp.com/signup", {
+            
+              user: this.userInfo,
+            },{
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods":
+            "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers":
+            "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length",
+          },
+        })
+        .then(
+          (response) => {
+            console.log(response);
+            this.$router.push("/");
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    },
+  },
+};
+</script>
+<style scoped>
+.main-layout{
+  height: 100vh;
+  width: 100vw;
+}
+.right-nav {
+  width: 100%;
+  height: 100%;
+  display: flex;
+flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+}
+.auth-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+a {
+  text-decoration: none;
+  color: #45a622;
+  transition: 0.5s ease;
+}
+a:hover {
+  color: #dfd3d3;
+}
+
+.btn {
+  border: 0;
+  border-radius: 0;
+  padding: 15px 0;
+  font-size: 3rem;
+  font-weight: 300;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+  color: #fff;
+  transition: all 0.5s ease;
+  -webkit-appearance: none;
+}
+.btn:hover,
+.btn:focus {
+  background: #fff;
+  color: #fff;
+}
+.btn1 {
+  padding: 15px 0;
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: #fff !important;
+  border-radius: 4px solid #ec6382;
+}
+.login {
+    display: flex;
+    flex-direction: column;
+  background: #fff;
+  border: 1px solid rgb(228, 214, 214);
+  padding: 30px;
+  max-width: 500px;
+  margin: 0 14px;
+  
+}
+
+
+.text-white input {
+  color: white !important;
+}
+.inp {
+  height: 40px;
+}
+</style>
